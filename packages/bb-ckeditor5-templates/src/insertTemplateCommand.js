f881c531fa188937e297b643daafa37d2112f64b
Akash Sarode <akash.sarode@media4u.com>
2021-10-01 17:30:09 +0530

    added acoms ckeditor build, and templates plugin

---
+++ packages/bb-ckeditor5-templates/src/insertTemplateCommand.js
@@ -0,0 +1,12 @@
+import { Command } from 'ckeditor5/src/core';
+
+export default class InsertTemplateCommand extends Command {
+	execute( content ) {
+		const editor = this.editor;
+
+		const viewFragment = editor.data.processor.toView( content );
+		const modelFragment = editor.data.toModel( viewFragment );
+
+		editor.model.insertContent( modelFragment );
+	}
+}
---
+++ packages/bb-ckeditor5-templates/src/templatesUi.js
@@ -0,0 +1,60 @@
+/* eslint-disable dot-notation */
+import { Plugin } from 'ckeditor5/src/core';
+import { Collection } from 'ckeditor5/src/utils';
+import { createDropdown, SplitButtonView, Model, addListToDropdown } from 'ckeditor5/src/ui';
+import TemplateIcon from '../icons/template.svg';
+
+export default class TemplatesUI extends Plugin {
+	init() {
+		this.editor.ui.componentFactory.add( 'templates', locale => {
+			// default entry in templates dropdown
+			const defaultEntry = [ {
+				id: 'default',
+				title: 'No templates available at the moment'
+			} ];
+
+			// templates supplied through editor config
+			let templates = this.editor.config.get( 'templates' ) || [];
+			templates = templates.length > 0 ? templates : defaultEntry;
+
+			// dropdown button in toolbar
+			const dropdownView = createDropdown( locale, SplitButtonView );
+
+			dropdownView.buttonView.set( {
+				withText: false,
+				label: 'Templates',
+				tooltip: true,
+				icon: TemplateIcon
+			} );
+
+			const items = new Collection();
+
+			items.addMany( templates.map( template => ( {
+				type: 'button',
+				model: new Model( {
+					withText: true,
+					id: template[ 'id' ] || template.title,
+					label: template.title
+				} )
+			} ) ) );
+
+			addListToDropdown( dropdownView, items );
+
+			dropdownView.on( 'execute', event => {
+				const id = event.source.id;
+
+				// stop event and return on default template
+				if ( id === 'default' ) {
+					event.stop();
+					return;
+				}
+
+				const template = templates.find( template => template.title === id );
+
+				this.editor.execute( 'insertTemplate', template[ 'html' ] );
+			} );
+
+			return dropdownView;
+		} );
+	}
+}
