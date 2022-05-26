a921b72fe0167d837410980fe50c7e3fdc8c9d19
rohit.sonje <rohit.sonje@media4u.com>
2022-05-26 23:12:40 +0530

    update template plugin dependency path and webpack config to create 2 editor

--- packages/bb-ckeditor5-build-acoms/webpack.config.js
+++ packages/bb-ckeditor5-build-acoms/webpack.config.js
@@ -3,95 +3,109 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
- 'use strict';
+'use strict';
 
- /* eslint-env node */
- 
- const path = require( 'path' );
- const webpack = require( 'webpack' );
- const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
- const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
- const TerserPlugin = require( 'terser-webpack-plugin' );
- 
- module.exports = {
-	 devtool: 'source-map',
-	 performance: { hints: false },
- 
-	 entry: path.resolve( __dirname, 'src', 'ckeditor.js' ),
- 
-	 output: {
-		 // The name under which the editor will be exported.
-		 library: 'CKEditorCS',
- 
-		 path: path.resolve( __dirname, 'build' ),
-		 filename: 'ckeditor.js',
-		 libraryTarget: 'umd',
-		 libraryExport: 'default'
-	 },
- 
-	 optimization: {
-		 minimizer: [
-			 new TerserPlugin( {
-				 sourceMap: true,
-				 terserOptions: {
-					 output: {
-						 // Preserve CKEditor 5 license comments.
-						 comments: /^!/
-					 }
-				 },
-				 extractComments: false
-			 } )
-		 ]
-	 },
- 
-	 plugins: [
-		 new CKEditorWebpackPlugin( {
-			 // UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
-			 // When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
-			 language: 'en',
-			 additionalLanguages: 'all'
-		 } ),
-		 new webpack.BannerPlugin( {
-			 banner: bundler.getLicenseBanner(),
-			 raw: true
-		 } )
-	 ],
- 
-	 module: {
-		 rules: [
-			 {
-				 test: /\.svg$/,
-				 use: [ 'raw-loader' ]
-			 },
-			 {
-				 test: /\.css$/,
-				 use: [
-					 {
-						 loader: 'style-loader',
-						 options: {
-							 injectType: 'singletonStyleTag',
-							 attributes: {
-								 'data-cke': true
-							 }
-						 }
-					 },
-					 {
-						 loader: 'css-loader'
-					 },
-					 {
-						 loader: 'postcss-loader',
-						 options: {
-							 postcssOptions: styles.getPostCssConfig( {
-								 themeImporter: {
-									 themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
-								 },
-								 minify: true
-							 } )
-						 }
-					 }
-				 ]
-			 }
-		 ]
-	 }
- };
- 
+/* eslint-env node */
+
+const path = require( 'path' );
+const webpack = require( 'webpack' );
+const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
+const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
+const TerserPlugin = require( 'terser-webpack-plugin' );
+
+const config = {
+	devtool: 'source-map',
+	performance: { hints: false },
+
+	optimization: {
+		minimizer: [
+			new TerserPlugin( {
+				sourceMap: true,
+				terserOptions: {
+					output: {
+						// Preserve CKEditor 5 license comments.
+						comments: /^!/
+					}
+				},
+				extractComments: false
+			} )
+		]
+	},
+
+	plugins: [
+		new CKEditorWebpackPlugin( {
+			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
+			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
+			language: 'en',
+			additionalLanguages: 'all'
+		} ),
+		new webpack.BannerPlugin( {
+			banner: bundler.getLicenseBanner(),
+			raw: true
+		} )
+	],
+
+	module: {
+		rules: [
+			{
+				test: /\.svg$/,
+				use: [ 'raw-loader' ]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: 'style-loader',
+						options: {
+							injectType: 'singletonStyleTag',
+							attributes: {
+								'data-cke': true
+							}
+						}
+					},
+					{
+						loader: 'css-loader'
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: styles.getPostCssConfig( {
+								themeImporter: {
+									themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
+								},
+								minify: true
+							} )
+						}
+					}
+				]
+			}
+		]
+	}
+};
+
+module.exports = [
+	// The first bundle will have the editor with watchdog and can be used in your application.
+	{
+		...config,
+		entry: path.resolve( __dirname, 'src', 'ckeditor.js' ),
+		output: {
+			library: 'Watchdog',
+			path: path.resolve( __dirname, 'build' ),
+			filename: 'ckeditor.js',
+			libraryTarget: 'umd',
+			libraryExport: 'default'
+		}
+	},
+	// The second bundle will be ready to be uploaded to the Cloud Services server.
+	{
+		...config,
+		entry: path.resolve( __dirname, 'src', 'ckeditor-cs.js' ),
+		output: {
+			library: 'CKEditorCS',
+			path: path.resolve( __dirname, 'build' ),
+			filename: 'ckeditor-cs.js',
+			libraryTarget: 'umd',
+			libraryExport: 'default'
+		}
+	}
+];
